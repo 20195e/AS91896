ba17db6abe7f71ca21bf6445fb8b80910e811f7b
20195e <100391354+20195e@users.noreply.github.com>
2023-08-22 23:06:09 +0000

    Minor Improvements.

--- store.py
+++ store.py
@@ -1,14 +1,43 @@
 #AS91893 / Price Compaison Tool.
+#I have added a way to make my text appear different
+class color:
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 name = input ("What's your name?")
 if name == name:
-  print ("Hello,", name,", Welcome to the price checker.")
+  print ("Hello," + name + ", Welcome to the Price Checker 1.0.")
 #I have added an optional set of instructions to assist in helping the user to understand how to use the program as intended.
-instructions = input ("\n\nWould you like some basic instructions on how to use this program?").lower()
+def Instructions():
+  print(color.CYAN + "\nWould you like some basic instructions on how to use this program?" + color.END)
+  ans1 = input("Please answer with Yes or No.").lower()
 
-if instructions == 'yes':
-  print ("ok")
-elif instructions == 'no':
-  print ("nuh uh")
-else:
-  print ("Please say either Yes or No")
-  print ()
+  if ans1 == "yes":
+    print ("1. Enter the your budget when prompted to, don't use a dollar sign.\n2. You may only use upto $10,000\n3.The program will then check the price of your selected items and recomend an item based on said budget.")
+    budget()
+
+  elif ans1 == "no":
+    print ("Okay!")
+    budget()
+
+  else:
+    print("Please answer Yes or No.")
+    Instructions()
+#I have defined my budget 
+def budget():
+  print("Please enter your budget.")
+  amount = float(input(""))
+
+  if amount <=0:
+   print("Please input either a valid number or one greater than 0.")
+
+  elif amount >100:
+    print ("Pleae enter a number below 100")
+
+  else:
+    print("Your budget is", amount)
+    
+Instructions() 
--- testingstuff.py
+++ testingstuff.py
@@ -1,2 +1,8 @@
 def products():
-  print ("1. Cheese\n2. Rice Cracker")
+  print ("1. Mainland Cheese = $10.00\n",
+         "2. Fantastic Rice Crackers = $2.40\n",
+         "3. Arnott's Shapes = $3.00\n",
+         "4. Anchor Milk = $12.00\n",
+         "5. Greek Yogurt = $6.00\n",
+         "6. Free range eggs = $9.00")
+  
